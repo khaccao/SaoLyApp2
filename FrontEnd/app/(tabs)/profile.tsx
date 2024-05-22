f7bb84ccfd7d67532a7f81c492f7271c40aece21
datpq28 <phamquydat2801@gmail.com>
2024-05-22 16:17:57 +0700

    create profile

--- FrontEnd/app/(tabs)/profile.tsx
+++ FrontEnd/app/(tabs)/profile.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Platform, View, Text, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Platform, View, Text, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Profile() {
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             {/* Header */}
             <View style={styles.header}>
                 <Image
@@ -27,25 +30,117 @@ export default function Profile() {
                 </View>
             </View>
 
-            {/* User Information */}
-            <View style={styles.userInfo}>
-                <Text style={styles.userName}>Tuan Ngo Anh</Text>
-                <Text style={styles.userLevel}>Bạc</Text>
-                <TouchableOpacity style={styles.exchangeButton}>
-                    <Text style={styles.exchangeText}>Đổi 999 đ</Text>
-                </TouchableOpacity>
-                <View style={styles.barcode}>
-                    <Text style={styles.barcodeText}>0123456789</Text>
+            {/* Background behind userInfoContainer */}
+            <View style={styles.backgroundContainer}>
+                {/* User Information */}
+                <View style={styles.userInfoContainer}>
+                    <View style={styles.userInfo}>
+                        <View style={styles.userDetails}>
+                            <Text style={styles.userName}>Tuan Ngo Anh</Text>
+                            <Text style={styles.userLevel}>Bạc</Text>
+                        </View>
+                        <TouchableOpacity style={styles.exchangeButton}>
+                            <Text style={styles.exchangeText}>Đổi 999 đ</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.barcode}>
+                        <Text style={styles.barcodeText}>0123456789</Text>
+                    </View>
                 </View>
             </View>
-        </View>
+            <View style={styles.accountContainer}>
+                <Text style={styles.accountTitle}>Tài khoản</Text>
+                <View style={styles.accountButtons}>
+                    <TouchableOpacity style={styles.accountButton}>
+                        <FontAwesome name="user-o" size={24} color="black" />
+                        <Text style={styles.buttonText}>Thông tin tài khoản</Text>
+                        <View style={styles.iconContainer}>
+                            <AntDesign name="right" size={20} color="black" />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.accountButton}>
+                        <MaterialIcons name="library-books" size={24} color="black" />
+                        <Text style={styles.buttonText}>Khoá học của tôi</Text>
+                        <View style={styles.iconContainer}>
+                            <AntDesign name="right" size={20} color="black" />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.accountTitle}>Túi đồ</Text>
+                <View style={styles.accountButtons}>
+                    <TouchableOpacity style={styles.accountButton}>
+                        <MaterialCommunityIcons name="ticket-confirmation-outline" size={24} color="black" />
+                        <Text style={styles.buttonText}>Voucher</Text>
+                        <View style={styles.iconContainer}>
+                            <AntDesign name="right" size={20} color="black" />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.accountButton}>
+                        <Feather name="box" size={24} color="black" />
+                        <Text style={styles.buttonText}>Đơn hàng</Text>
+                        <View style={styles.iconContainer}>
+                            <AntDesign name="right" size={20} color="black" />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.accountButton}>
+                        <Feather name="heart" size={24} color="black" />
+                        <Text style={styles.buttonText}>Danh sách yêu thích</Text>
+                        <View style={styles.iconContainer}>
+                            <AntDesign name="right" size={20} color="black" />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.accountButton}>
+                        <MaterialCommunityIcons name="script-text-outline" size={24} color="black" />
+                        <Text style={styles.buttonText}>Hoá đơn của tôi</Text>
+                        <View style={styles.iconContainer}>
+                            <AntDesign name="right" size={20} color="black" />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.accountTitle}>Săn thưởng</Text>
+                <View style={styles.accountButtons}>
+                    <TouchableOpacity style={styles.accountButton}>
+                        <MaterialCommunityIcons name="ticket-percent-outline" size={24} color="black" />
+                        <Text style={styles.buttonText}>Mã ưu đãi</Text>
+                        <View style={styles.iconContainer}>
+                            <AntDesign name="right" size={20} color="black" />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.accountButton}>
+                        <AntDesign name="addusergroup" size={24} color="black" />
+                        <Text style={styles.buttonText}>Giới thiệu bạn bè</Text>
+                        <View style={styles.iconContainer}>
+                            <AntDesign name="right" size={20} color="black" />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.accountTitle}>Khác</Text>
+                <View style={styles.accountButtons}>
+                    <TouchableOpacity style={styles.accountButton}>
+                        <MaterialIcons name="contact-support" size={24} color="black" />
+                        <Text style={styles.buttonText}>Hỗ trợ</Text>
+                        <View style={styles.iconContainer}>
+                            <AntDesign name="right" size={20} color="black" />
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.accountButton}>
+                        <Feather name="settings" size={24} color="black" />
+                        <Text style={styles.buttonText}>Tuỳ chỉnh hệ thống</Text>
+                        <View style={styles.iconContainer}>
+                            <AntDesign name="right" size={20} color="black" />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#FDEFDD',
     },
     header: {
         flexDirection: 'row',
@@ -53,11 +148,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingTop: Platform.OS === 'ios' ? 50 : 40,
         paddingHorizontal: 15,
-        backgroundColor: '#F9F9F9',
+        backgroundColor: '#FDEFDD',
     },
     logo: {
-        width: 200,
+        width: 230,
         height: 50,
+        marginLeft: -50,  // Dịch logo sang trái
         ...Platform.select({
             ios: {
                 shadowColor: '#000',
@@ -95,7 +191,7 @@ const styles = StyleSheet.create({
         }),
     },
     iconButton: {
-        marginLeft: 20,
+        marginLeft: 10, // Giảm khoảng cách giữa biểu tượng thông báo và vé
         alignItems: 'center',
     },
     iconText: {
@@ -105,8 +201,8 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     notificationCircle: {
-        width: 40,
-        height: 40,
+        width: 35,
+        height: 35,
         borderRadius: 20,
         backgroundColor: '#fff',
         justifyContent: 'center',
@@ -130,8 +226,8 @@ const styles = StyleSheet.create({
         right: -5,
         backgroundColor: 'red',
         borderRadius: 10,
-        width: 20,
-        height: 20,
+        width: 18,
+        height: 18,
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -140,36 +236,57 @@ const styles = StyleSheet.create({
         fontSize: 10,
         fontWeight: 'bold',
     },
-    userInfo: {
-        marginTop: 20,
+    backgroundContainer: {
+        width:'100%',
+        height: 215,
+        backgroundColor: '#FDEFDD',
         paddingHorizontal: 20,
+    },
+    userInfoContainer: {
+        backgroundColor: '#A1ADCA',
+        padding: 20,
+        borderRadius: 15,
+        marginVertical: 20,
         alignItems: 'center',
+        height: 180,
+    },
+    userInfo: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+    },
+    userDetails: {
+        flexDirection: 'column',
     },
     userName: {
-        fontSize: 24,
+        fontSize: 18,
         fontWeight: 'bold',
+        color: '#fff',
     },
     userLevel: {
-        fontSize: 16,
-        color: '#888',
-        marginVertical: 10,
+        fontSize: 14,
+        color: '#fff',
+        marginVertical: 5,
     },
     exchangeButton: {
-        backgroundColor: '#E0E0E0',
-        borderRadius: 10,
-        paddingVertical: 5,
-        paddingHorizontal: 10,
-        marginVertical: 10,
+        backgroundColor: '#E9DCCB',
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+        paddingVertical: 9,
+        paddingHorizontal: 31,
+        marginLeft: 98,
     },
     exchangeText: {
-        fontSize: 16,
-        color: 'black',
+        fontSize: 14,
+        color: '#fff',
+        fontWeight: 'bold'
     },
     barcode: {
         marginTop: 20,
         backgroundColor: 'white',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
+        paddingVertical: 20,
+        paddingHorizontal: 70,
         borderRadius: 10,
         borderWidth: 1,
         borderColor: '#E0E0E0',
@@ -177,5 +294,42 @@ const styles = StyleSheet.create({
     barcodeText: {
         fontSize: 20,
         letterSpacing: 5,
+    },
+
+    accountContainer: {
+        marginTop: 10,
+        paddingHorizontal: 20,
+        backgroundColor:'#fff'
+    },
+    accountTitle: {
+        marginTop: 10,
+        fontSize: 14,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    accountButtons: {
+        marginTop: 10,
+        flexDirection: 'column',
+        marginBottom: 10,
+    },
+    accountButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#EEEEEE',
+        padding: 10,
+        elevation: 3,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.3,
+        shadowRadius: 2,
+    },
+    buttonText: {
+        marginLeft: 20,
+        fontSize: 14,
+    },
+    iconContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
     },
 });
